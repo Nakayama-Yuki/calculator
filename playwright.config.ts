e59875281daadf661d1714f064c0e62c67847c96
yuki <yuki.for.neutral@gmail.com>
2025-12-16 15:08:48 +0900

    fix: テストファイルを配置するディレクトリのコメントの全角スペースを削除

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  //　テストファイルを配置するディレクトリ
+  // テストファイルを配置するディレクトリ
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
